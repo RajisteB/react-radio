92ef9db10bf6eabacd02cec136b99c3656dda8c8
Raj Bennin <rajisteb@gmail.com>
2018-05-14 00:33:51 -0400

    add all genres

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import xml from 'xml2js';
 import './App.css';
 const castKey = process.env.REACT_APP_SHOUTCAST_KEY;
 const stationUrl = process.env.REACT_APP_SHOUTCAST_STATION_URL;
+const genres = [
+  'classical', 'jazz', 'blues', 'rap', 
+  'country', 'alternative', 'pop', 'talk', 
+  'latin', 'reggae', 'r%26b', 'decades', 
+  'new age', 'international', 'electronic', 'metal',
+  'folk', 'easy listening', 'inspirational', 'public radio'
+];
 
 class App extends Component {
   constructor() {
@@ -16,19 +23,28 @@ class App extends Component {
   }
 
   getBroadcast = () => {
-    let points = [];
-    axios.get(`${stationUrl}classical${castKey}`)
-    .then(res => {
-      let data = res.data;
-      xml.parseString(data, (err, result) => {
-        console.log(result);
-        result.stationlist.station.map(radio => radio['$'].streaming = false);
-        this.setState({
-          data: result.stationlist
+    genres.map(genre => {
+      let points = [];
+      axios.get(`${stationUrl}${genre}${castKey}`)
+      .then(res => {
+        let data = res.data;
+
+        // convert xml response to json
+        xml.parseString(data, (err, result) => {
+          console.log(result);
+
+          // add a boolean to response object
+          result.stationlist.station.map(radio => radio['$'].streaming = false);
+
+          // dynamically create different setState key for each array item/genre
+          this.setState({
+            [genre]: result.stationlist
+          })
         })
+
+      }).catch(err => {
+        console.log(err);
       })
-    }).catch(err => {
-      console.log(err);
     })
   }
 
@@ -37,6 +53,7 @@ class App extends Component {
   }
 
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
@@ -44,7 +61,11 @@ class App extends Component {
           <h1 className="App-title">React Radio</h1>
         </header>
         <div className="App-intro">
-          <StationList title="Classical" stations={this.state.data} />
+          {genres.map(genre => {
+            let title = genre.charAt(0).toUpperCase() + genre.substr(1);
+            return <StationList title={title} stations={this.state[genre]} />
+          })}
+          {/* <StationList title="Classical" stations={this.state.data} /> */}
         </div>
       </div>
     );
